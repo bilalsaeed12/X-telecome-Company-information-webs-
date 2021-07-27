fb14d925a1875df480a85950d0d7de9eda207017
bilalsaeed12 <bilalsaeed128955@gmail.com>
2021-07-27 18:37:26 +0500

    My second commit

---
+++ X-telecom/Home.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Basic -->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta charset="utf-8">
+    <title>Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ 
+  <meta name="keywords" content="footer, address, phone, icons" />
+
+ 
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Site CSS -->
+    <link rel="stylesheet" href="css1/style.css">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
+  
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+
+  <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+    
+
+</head>
+
+<header>
+	
+	<div class="row ">
+
+
+
+    <div class="col-lg-12" style="background-color:#0088cc; height:3px ; ">
+
+    </div>
+</div>
+  <nav  class="navbar navbar-expand-sm navbar sticky-top     justify-content-between">
+
+   <div  style="color: black; font-size: 17px;font-weight: bold;margin-left: 5%"> 
+
+
+<a href="#" style="color: black;text-decoration: none;">
+   <i class="fa fa-angle-right" style="font-size:15px"></i>&nbsp; ABOUT US</div>
+
+ 
+
+</a>
+
+
+
+
+  <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent"> -->
+
+  	<div class="container">
+
+
+
+    <ul class="navbar-nav ml-auto" >
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html" style="color: black;font-weight: bold">Contact Us <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="mailto:h.abdulbasit975@gmail.com" style="color: black;font-weight: bold">
+
+
+<i class="far fa-envelope" style="color: #0088cc;font-size: 20px;"></i>   &nbsp;support@x-telecom.biz
+
+</a>
+      </li>
+     
+      <li class="nav-item">
+     <a class="nav-link" href="tel:+92203076838042"style="color: black;font-weight: bold">
+
+
+
+ <i class="fab fa-whatsapp" style="color: #0088cc;font-size: 20px;"></i>  &nbsp;0305-4771027
+
+</a>      </li>
+
+     
+
+    </ul>
+   </div>
+  </div>
+</nav>
+</header>
+    <header class="main-header">
+        <!-- Start Navigation -->
+        <nav style="background-color: white" class="navbar navbar-expand-lg  navbar-default bootsnav">
+                          <!-- Start Header Navigation -->
+                <div class="navbar-header">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-menu" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa fa-bars"></i>
+                </button>
+                    <a class="navbar-brand" href="index.html"  ><img  src="images/logo2.png" class="logo" alt="" height="50%" width="50%" style="margin-left: 20%"></a>
+                </div>
+                <!-- End Header Navigation -->
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="navbar-menu">
+                    <ul  class="nav navbar-nav ml-auto" data-in="fadeInDown" data-out="fadeOutUp">
+                        <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
+                    <li class="nav-item "><a class="nav-link" href="about.html">Services</a></li>
+                       <li class="nav-item"><a class="nav-link" href="gallery.html">Products</a></li>
+                        <li class="nav-item"><a class="nav-link" href="gallery.html">Final Expense</a></li>
+                        <li class="nav-item"><a class="nav-link" href="gallery.html">Pricing & Orders</a></li>
+
+                        <li class="nav-item"><a class="nav-link" href="contact-us.html">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="contact-us.html">Testinomianls</a></li>
+
+  <li class="nav-item"><a class="nav-link"  href="contact-us.html">
+                      <i class="fab fa-facebook"></i> 
+                  </a></li>
+
+                  
+
+                   <li class="nav-item"><a class="nav-link"  href="contact-us.html">
+                      <i class="fab fa-linkedin"></i>
+                      <!-- <i class="fab fa-facebook"></i> -->
+                  </a>
+
+                   </li>
+
+                  
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+
+                <!-- Start Atribute Navigation -->
+              
+                <!-- End Atribute Navigation -->
+      
+            <!-- Start Side Menu -->
+         
+            <!-- End Side Menu -->
+        </nav>
+        <!-- End Navigation -->
+    </header>
+
+
+<button onclick="topFunction()" id="myBtn" title="Go to top">
+  
+    <a class="gotopbtn" href="#"> <i class="fas fa-arrow-up" style="color: white"></i> </a>
+
+
+
+
+</button>
+
+ 
+<script>
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+</script>
+  
+
+	
+</body>
+
+
+</html>
